1ef2e132d0465f1bf5f5c071dbc3f1b121f7c63d
thearrasoverlord-netizen <thearrasoverlord@gmail.com>
2026-09-20 19:58:40 -0300

    Add Open Workshop panel

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,37 @@
 using namespace geode::prelude;
 
 class $modify(DecorationWorkshopEditorUI, EditorUI) {
-    void onDecorationWorkshopInfo(CCObject*) {
-        FLAlertLayer::create(
-            "Decoration Workshop GD",
-            "Decoration Workshop GD is an editor tool focused on making level decoration faster and easier.",
-            "OK"
-        )->show();
+    void onWorkshopClose(CCObject*) {
+        if (auto panel = this->getChildByTag(98765)) {
+            panel->removeFromParent();
+        }
+    }
+
+    void onWorkshopOpen(CCObject*) {
+        if (this->getChildByTag(98765))
+            return;
+
+        auto panel = CCLayerColor::create(ccc4(125, 82, 45, 255), 520.0f, 320.0f);
+        panel->setTag(98765);
+        panel->setPosition(
+            (this->getContentSize().width - panel->getContentSize().width) / 2.0f,
+            (this->getContentSize().height - panel->getContentSize().height) / 2.0f
+        );
+        this->addChild(panel, 100);
+
+        auto closeSprite = CCLabelBMFont::create("X", "bigFont.fnt");
+        closeSprite->setScale(0.65f);
+
+        auto closeButton = CCMenuItemSpriteExtra::create(
+            closeSprite,
+            this,
+            menu_selector(DecorationWorkshopEditorUI::onWorkshopClose)
+        );
+
+        auto closeMenu = CCMenu::create();
+        closeMenu->setPosition(24.0f, panel->getContentSize().height - 24.0f);
+        closeMenu->addChild(closeButton);
+        panel->addChild(closeMenu);
     }
 
     bool init(LevelEditorLayer* editorLayer) {
@@ -20,19 +45,19 @@ class $modify(DecorationWorkshopEditorUI, EditorUI) {
         alpha::editor_tabs::addTab(
             "decoration-workshop"_spr,
             alpha::editor_tabs::BUILD,
-            [] {
+            [this] {
                 std::vector<Ref<CCNode>> nodes;
 
-                auto infoSprite = CCLabelBMFont::create("i", "bigFont.fnt");
-                infoSprite->setScale(0.8f);
+                auto openSprite = CCLabelBMFont::create("[Open Workshop]", "bigFont.fnt");
+                openSprite->setScale(0.45f);
 
-                auto infoButton = CCMenuItemSpriteExtra::create(
-                    infoSprite,
-                    nullptr,
-                    nullptr
+                auto openButton = CCMenuItemSpriteExtra::create(
+                    openSprite,
+                    this,
+                    menu_selector(DecorationWorkshopEditorUI::onWorkshopOpen)
                 );
 
-                nodes.push_back(infoButton);
+                nodes.push_back(openButton);
 
                 return alpha::editor_tabs::createEditButtonBar(nodes);
             },
