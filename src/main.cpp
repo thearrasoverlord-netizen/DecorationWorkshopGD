7b76c2e5a647f027366646c5b5c87020d58e7b97
thearrasoverlord-netizen <thearrasoverlord@gmail.com>
2026-09-20 19:44:34 -0300

    Add Decoration Workshop editor tab

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,40 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/EditorUI.hpp>
+#include <alphalaneous.editortab_api/include/EditorTabAPI.hpp>
 
 using namespace geode::prelude;
 
-class $modify(DecorationWorkshopMenuLayer, MenuLayer) {
-    void onDecorationWorkshopPopup(float) {
+class $modify(DecorationWorkshopEditorUI, EditorUI) {
+    void onDecorationWorkshopInfo(CCObject*) {
         FLAlertLayer::create(
             "Decoration Workshop GD",
-            "The mod is working!",
+            "Decoration Workshop GD is an editor tool focused on making level decoration faster and easier.",
             "OK"
         )->show();
     }
 
-    bool init() {
-        if (!MenuLayer::init())
+    bool init(LevelEditorLayer* editorLayer) {
+        if (!EditorUI::init(editorLayer))
             return false;
 
-        this->scheduleOnce(
-            schedule_selector(DecorationWorkshopMenuLayer::onDecorationWorkshopPopup),
-            0.5f
+        alpha::editor_tabs::addTab(
+            "decoration-workshop"_spr,
+            alpha::editor_tabs::BUILD,
+            [this] {
+                auto infoSprite = CCLabelBMFont::create("i", "bigFont.fnt");
+                infoSprite->setScale(0.8f);
+
+                auto infoButton = CCMenuItemSpriteExtra::create(
+                    infoSprite,
+                    this,
+                    menu_selector(DecorationWorkshopEditorUI::onDecorationWorkshopInfo)
+                );
+
+                return alpha::editor_tabs::createEditButtonBar({infoButton});
+            },
+            [] {
+                return CCLabelBMFont::create("DW", "bigFont.fnt");
+            }
         );
 
         return true;
